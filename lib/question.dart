04ef57beb3f66cbb3ce496af576d7284ac6acb24
Marco <marco.porcaro@synesthesia.it>
2022-09-30 14:37:05 +0200

    Part 1

---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+
+class Question {
+
+  final String text;
+  final List<String> answers;
+  final String correctAnswer;
+
+  Question({required this.text, required this.answers, required this.correctAnswer});
+
+}
